4abd117404f367fd8c24060e52f584e2dfe60986
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2022-03-19 12:55:40 -0300

    AppResponsivo-1

--- lib/pages/home/widgets/sections/top_sections.dart
+++ lib/pages/home/widgets/sections/top_sections.dart
@@ -1,3 +1,4 @@
+import 'package:app1/breakpoints.dart';
 import 'package:app1/pages/home/widgets/custom_search_field.dart';
 import 'package:flutter/material.dart';
 
@@ -31,6 +32,57 @@ class TopSection extends StatelessWidget {
                     child: Container(
                       padding: const EdgeInsets.all(22),
                       width: 450,
+                      child: Column(
+                        children: const [
+                          Text(
+                            "Aprenda Flutter com este curso",
+                            style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            "Bora aprender Flutter com o professor! Curso por apenas R\$22,99. Qualidade Garantida.",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 16),
+                          CustomSearchField(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+
+        if (maxWidth >= mobileBreakpoint) {
+          return SizedBox(
+            height: 320,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: 250,
+                  width: double.infinity,
+                  child: Image.network(
+                    "https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  left: 50,
+                  top: 50,
+                  child: Card(
+                    color: Colors.black,
+                    elevation: 8,
+                    child: Container(
+                      padding: const EdgeInsets.all(22),
+                      width: 350,
                       child: Column(
                         children: const [
                           Text(
